0d350c01fe0b5de3b6a7a694769fe1e4d7bc1150
Jaewoo <sjwlion@naver.com>
2025-06-17 12:54:42 +0900

    utils.py

--- src/.ipynb_checkpoints/utils-checkpoint.py
+++ src/.ipynb_checkpoints/utils-checkpoint.py
@@ -80,7 +80,7 @@ def col_value_counts(df, column):
     print(df[column].value_counts())
 
 # 여러개의 함수 Plot 
-def plot_multiple_axes(df, cols, plot_type='hist', target=None, n_cols=3, height=4, bins=30, xrot=0, exclude_cols=None):
+def plot_multiple_axes(df, cols, plot_type='hist', target=None, n_cols=3, height=4, bins=30, xrot=0, exclude_cols=None, hue=None):
     """
     여러 피처의 시각화를 한 번에 출력하는 범용 함수
     특정 피처를 제외하고 시각화할 수 있는 기능을 추가.
@@ -121,13 +121,16 @@ def plot_multiple_axes(df, cols, plot_type='hist', target=None, n_cols=3, height
         if plot_type == 'hist':
             sns.histplot(data=df, x=col, bins=bins, ax=ax) #, hue=col, palette='pastel', legend=False)
         elif plot_type == 'count':
-            sns.countplot(data=df, x=col, ax=ax, hue=col, palette='pastel', legend=False)
+            sns.countplot(data=df, x=col, ax=ax)
         elif plot_type == 'bar' and target:
-            sns.barplot(data=df, x=col, y=target, ax=ax, hue=col, palette='pastel', legend=False)
+            sns.barplot(data=df, x=col, y=target, ax=ax)
         elif plot_type == 'box' and target:
-            sns.boxplot(data=df, x=target, y=col, ax=ax, hue=col, palette='pastel', legend=False)
+            if hue:
+                sns.boxplot(data=df, x=target, y=col, ax=ax, hue=hue, palette='pastel')
+            else:
+                sns.boxplot(data=df, x=target, y=col, ax=ax)
         elif plot_type == 'violin' and target:
-            sns.violinplot(data=df, x=target, y=col, ax=ax, hue=col, palette='pastel', legend=False)
+            sns.violinplot(data=df, x=target, y=col, ax=ax)
         else:
             ax.text(0.5, 0.5, 'Invalid plot_type or missing target', ha='center')
 
